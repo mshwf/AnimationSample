aac30d14bb41af415d2c3bcea010a8796d1ad0e3
mshwf <dt4it@hotmail.com>
2019-02-28 09:13:18 +0200

    .

--- AnimationSample/AnimationSample/AnimationPage.xaml.cs
+++ AnimationSample/AnimationSample/AnimationPage.xaml.cs
@@ -12,7 +12,7 @@ namespace AnimationSample
         }
         bool b1Visible = true;
         bool b2Visible = true;
-        double filterHeight = 150;
+        double b2Height = 150;
 
         private async void FadeB1_Clicked(object sender, EventArgs e)
         {
@@ -24,12 +24,12 @@ namespace AnimationSample
             Animation animation;
             if (b2Visible)
             {
-                animation = new Animation(v => box2.HeightRequest = v, filterHeight, 0, easing: Easing.SinOut);
+                animation = new Animation(v => box2.HeightRequest = v, b2Height, 0, easing: Easing.SinOut);
                 box2.Animate("b2", animation);
             }
             else
             {
-                animation = new Animation(v => box2.HeightRequest = v, 0, filterHeight, easing: Easing.SinIn);
+                animation = new Animation(v => box2.HeightRequest = v, 0, b2Height, easing: Easing.SinIn);
                 box2.Animate("b2", animation);
             }
 

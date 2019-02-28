d8f0b9faca6a1a3224a10b0333da9a352af0b1c0
mshwf <dt4it@hotmail.com>
2019-02-28 09:06:23 +0200

    Add project files.

---
+++ AnimationSample/AnimationSample/AnimationPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AnimationSample
+{
+    public partial class AnimationPage : ContentPage
+    {
+
+        public AnimationPage()
+        {
+            InitializeComponent();
+        }
+        bool b1Visible = true;
+        bool b2Visible = true;
+        double filterHeight = 150;
+
+        private async void FadeB1_Clicked(object sender, EventArgs e)
+        {
+            await box1.FadeTo(b1Visible ? 0 : 1);
+            b1Visible = !b1Visible;
+        }
+        private void AniomateHeightB2_Clicked(object sender, EventArgs e)
+        {
+            Animation animation;
+            if (b2Visible)
+            {
+                animation = new Animation(v => box2.HeightRequest = v, filterHeight, 0, easing: Easing.SinOut);
+                box2.Animate("b2", animation);
+            }
+            else
+            {
+                animation = new Animation(v => box2.HeightRequest = v, 0, filterHeight, easing: Easing.SinIn);
+                box2.Animate("b2", animation);
+            }
+
+            b2Visible = !b2Visible;
+
+        }
+    }
+}
